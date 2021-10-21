2c426fc11514e54ee73f3d4a50f46e5a790c3da2
Stuti Suthar <stuti.suthar@sap.com>
2021-10-22 01:38:31 +0530

    Selenium test

--- bookstore-catalog-service/src/test/java/com/devd/spring/bookstorecatalogservice/BookstoreCatalogServiceApplicationTests.java
+++ bookstore-catalog-service/src/test/java/com/devd/spring/bookstorecatalogservice/BookstoreCatalogServiceApplicationTests.java
@@ -6,16 +6,11 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.remote.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class BookstoreCatalogServiceApplicationTests {
 
-  private static ChromeDriverService service;
-  private WebDriver driver;
-
   @Test
-  public void testGoogleSearch() {
-    driver.navigate().to("http://www.google.com");
-    System.out.println("Title : "+driver.getTitle());
-    driver.navigate().back();
-    // rest of the test...
-  }
+	public void contextLoads() {
+	}
 }
