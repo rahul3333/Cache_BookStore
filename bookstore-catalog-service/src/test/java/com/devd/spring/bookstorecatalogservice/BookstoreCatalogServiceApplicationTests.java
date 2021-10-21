49e333200d3619ebdb7b5ecb47858a1607b6ebce
Stuti Suthar <stuti.suthar@sap.com>
2021-10-22 01:29:16 +0530

    Test case updated

--- bookstore-catalog-service/src/test/java/com/devd/spring/bookstorecatalogservice/BookstoreCatalogServiceApplicationTests.java
+++ bookstore-catalog-service/src/test/java/com/devd/spring/bookstorecatalogservice/BookstoreCatalogServiceApplicationTests.java
@@ -11,30 +11,6 @@ public class BookstoreCatalogServiceApplicationTests {
   private static ChromeDriverService service;
   private WebDriver driver;
 
-  @BeforeClass
-  public static void createAndStartService() throws IOException {
-    service = new ChromeDriverService.Builder()
-        .usingDriverExecutable(new File("C:\\Users\\I524918\\Downloads\\chromedriver.exe"))
-        .usingAnyFreePort()
-        .build();
-    service.start();
-  }
-
-  @AfterClass
-  public static void stopService() {
-    service.stop();
-  }
-
-  @Before
-  public void createDriver() {
-    driver = new RemoteWebDriver(service.getUrl(), new ChromeOptions());
-  }
-
-  @After
-  public void quitDriver() {
-    driver.quit();
-  }
-
   @Test
   public void testGoogleSearch() {
     driver.navigate().to("http://www.google.com");
